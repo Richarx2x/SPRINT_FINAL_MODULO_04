42ccbae75f52037bffec2b38e78cbd96d07d8d3c
MauricioJMoralesV <129233232+MauricioJMoralesV@users.noreply.github.com>
2023-06-09 19:40:39 -0400

    Actualizacion IMPORT de Administrativo

--- Administrativo.java
+++ Administrativo.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 class Administrativo extends Usuario {
     private String area;
     private String experienciaPrevia;
