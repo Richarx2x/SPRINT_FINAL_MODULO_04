3f7c060cc1375d880ec08c92cf0c6269c1208799
Ricardo Cea <ricardocea.audio@gmail.com>
2023-06-09 01:00:15 -0400

    anado archivo validaciones (contiene validaciones de clases Accidente y Cliente)

--- src/Accidente.java
+++ src/Accidente.java
@@ -17,7 +17,12 @@ class Accidente {
     public Accidente(int identificador, int rutCliente, String dia, String hora, String lugar, String origen, String consecuencias) {
         this.identificador = identificador;
         this.rutCliente = rutCliente;
-        if (validarFormatoFecha(dia)){
+        this.dia = dia;
+        this.hora = hora;
+        this.lugar = lugar;
+        this.origen = origen;
+        this.consecuencias = consecuencias;
+/*        if (validarFormatoFecha(dia)){
             this.dia = dia;
         } else {System.out.println("No se reconoce la fecha ingresada");}
         if (validarFormatoHora(hora)) {
@@ -31,7 +36,7 @@ class Accidente {
         } else {System.out.println("No se reconoce el origen del accidente ingresado");}
         if (validarLongitudConsecuencias(consecuencias)) {
             this.consecuencias = consecuencias;
-        } else {System.out.println("No se reconocen las consecuencias ingresadas");}
+        } else {System.out.println("No se reconocen las consecuencias ingresadas");}*/
     }
 
     // Métodos de acceso para los atributos de Accidente
@@ -42,6 +47,9 @@ class Accidente {
 
     public void setIdentificador(int identificador) {
         this.identificador = identificador;
+/*        if (validarIdentificador(identificador)) {
+            this.identificador = identificador;
+        } else {System.out.println("Ingresa bien el ID. Identificador ingresado no es correcto");}*/
     }
 
     public int getRutCliente() {
@@ -50,6 +58,9 @@ class Accidente {
 
     public void setRutCliente(int rutCliente) {
         this.rutCliente = rutCliente;
+/*        if (validarRutAccidente(rut)) {
+            this.rutCliente = rutCliente;
+        } else {System.out.println("Ingresa bien el RUT. RUT ingresado no es correcto");}*/
     }
 
     public String getDia() {
@@ -57,10 +68,10 @@ class Accidente {
     }
 
     public void setDia(String dia) {
-        //this.dia = dia;
-        if (validarFormatoFecha(dia)){
+        this.dia = dia;
+/*        if (validarFormatoFecha(dia)){
             this.dia = dia;
-        } else {System.out.println("Ingresa bien la fecha compañero/a");}
+        } else {System.out.println("Ingresa bien la fecha: Formato DD/MM/AAAA");}*/
     }
 
     public String getHora() {
@@ -68,10 +79,10 @@ class Accidente {
     }
 
     public void setHora(String hora) {
-        // this.hora = hora;
-        if (validarFormatoHora(hora)){
+        this.hora = hora;
+/*        if (validarFormatoHora(hora)){
             this.hora = hora;
-        } else {System.out.println("Ingresa bien la hora compañero/a");}
+        } else {System.out.println("Ingresa bien la hora: Formato HH:MM");}*/
     }
 
     public String getLugar() {
@@ -79,10 +90,10 @@ class Accidente {
     }
 
     public void setLugar(String lugar) {
-        // this.lugar = lugar;
-        if (validarLongitudLugar(lugar)){
+        this.lugar = lugar;
+/*        if (validarLongitudLugar(lugar)){
             this.lugar = lugar;
-        } else {System.out.println("Ingresa bien el lugar del accidente. Min 10 caracteres, Max 50 caracteres");}
+        } else {System.out.println("Ingresa bien el lugar del accidente. Min 10 caracteres, Max 50 caracteres");}*/
     }
 
     public String getOrigen() {
@@ -90,10 +101,10 @@ class Accidente {
     }
 
     public void setOrigen(String origen) {
-        // this.origen = origen;
-        if (validarLongitudOrigen(origen)){
+        this.origen = origen;
+/*        if (validarLongitudOrigen(origen)){
             this.origen = origen;
-        } else {System.out.println("Ingresa bien el origen del accidente. Max 100 caracteres caracteres permitidos");}
+        } else {System.out.println("Ingresa bien el origen del accidente. Max 100 caracteres caracteres permitidos");}*/
     }
 
     public String getConsecuencias() {
@@ -101,10 +112,25 @@ class Accidente {
     }
 
     public void setConsecuencias(String consecuencias) {
-        // this.consecuencias = consecuencias;
-        if (validarLongitudConsecuencias(consecuencias)) {
+        this.consecuencias = consecuencias;
+/*        if (validarLongitudConsecuencias(consecuencias)) {
             this.consecuencias = consecuencias;
-        } else {System.out.println("Ingresa bien las consecuencias del accidente. Max 100 caracteres permitidos");}
+        } else {System.out.println("Ingresa bien las consecuencias del accidente. Max 100 caracteres permitidos");}*/
+    }
+
+    // VALIDADORES CLASS ACCIDENTE:
+/*    // VALIDADOR IDENTIFICADOR (ID)
+    public static boolean validarIdentificador(int identificador) {
+        String identificadorStr = Integer.toString(identificador);
+        String patron = "^[0-9]{1,}$";
+        return Pattern.matches(patron, identificadorStr);
+    }
+
+    // VALIDADOR RUT
+    public static boolean validarRutAccidente(int rut) {
+        String rutStr = Integer.toString(rut);
+        String patron = "^[0-9]{1,}$";
+        return Pattern.matches(patron, rutStr);
     }
 
     // VALIDADOR HORA
@@ -137,10 +163,29 @@ class Accidente {
         return consecuencias.length() <= longitudMaxima;
     }
 
+    // ----------------------------------------------------------------------------------
     // GENERAMOS ID
     private int generaId(){
         return (int) (Math.random() * 89999999) + 10000000;
+    }*/
+
+    // toString:
+    @Override
+    public String toString() {
+        return "Accidente{" +
+                "identificador=" + identificador +
+                ", rutCliente=" + rutCliente +
+                ", dia='" + dia + '\'' +
+                ", hora='" + hora + '\'' +
+                ", lugar='" + lugar + '\'' +
+                ", origen='" + origen + '\'' +
+                ", consecuencias='" + consecuencias + '\'' +
+                '}';
     }
 
+    //ANALIZAR USUARIO:
+    @Override
+    public void analizarUsuario(){
 
+    }
 }
--- src/Cliente.java
+++ src/Cliente.java
@@ -1,23 +1,28 @@
 //import java.util.Date;
 import java.time.LocalDate;
+import java.util.regex.Pattern;
+
 //import java.time.LocalTime;
 class Cliente extends Usuario {
+
+    // Declaración de variables
     private int rut;
     private String nombres;
     private String apellidos;
-    private String telefono;
+    private int telefono;
     private String afp;
     private int sistemaSalud;
     private String direccion;
     private String comuna;
     private int edad;
 
+    // Constructor vacio
     public Cliente() {}
-
-    public Cliente(String nombre, LocalDate fechaNacimiento, int run, int rut, String nombres, String apellidos, String telefono, String afp, int sistemaSalud, String direccion, String comuna, int edad) {
+    // Constructor con objetos clase padre + objetos propios
+    public Cliente(String nombre, String fechaNacimiento, int run, int rut, String nombres, String apellidos, int telefono, String afp, int sistemaSalud, String direccion, String comuna, int edad) {
         super(nombre, fechaNacimiento, run);
-        this.rut = rut; // ya está el run en la clase padre
-        this.nombres = nombres; // se podria considerar el de la clase padre?
+        this.rut = rut;
+        this.nombres = nombres;
         this.apellidos = apellidos;
         this.telefono = telefono;
         this.afp = afp;
@@ -25,9 +30,32 @@ class Cliente extends Usuario {
         this.direccion = direccion;
         this.comuna = comuna;
         this.edad = edad;
+/*        if (validarLongitudApellidos(apellidos)) {
+            this.apellidos = apellidos;
+        } else {System.out.println("No se reconocen los apellidos ingresados");}
+        if (validarTelefono(telefono)) {
+            this.telefono = telefono;
+        } else {System.out.println("No se reconoce el teléfono ingresado");}
+        if (validarLongitudAfp(afp)) {
+            this.afp = afp;
+        } else {System.out.println("No se reconoce la afp ingresada");}
+        if (validarSistemaSalud(sistemaSalud)) {
+            this.sistemaSalud = sistemaSalud;
+        } else {System.out.println("No se reconoce el sistema de salud ingresado");}
+        if (validarLongitudDireccion(direccion)) {
+            this.direccion = direccion;
+        } else {System.out.println("No se reconoce la dirección ingresada");}
+        if (validarLongitudComuna(comuna)) {
+            this.comuna = comuna;
+        } else {System.out.println("No se reconoce la comuna ingresada");}
+        if (validarEdad(edad)) {
+            this.edad = edad;
+        } else {System.out.println("No se reconoce la edad ingresada");}*/
+
     }
 
-    // Métodos de acceso para los atributos específicos de Cliente
+
+    // Métodos de acceso para los atributos específicos de Cliente (Getters & Setters)
 
     public int getRut() {
         return rut;
@@ -35,6 +63,9 @@ class Cliente extends Usuario {
 
     public void setRut(int rut) {
         this.rut = rut;
+/*        if (validarRut(int rut)) {
+            this.rut = rut;
+        } else {System.out.println("Ingresa bien el RUT. Utiliza número menor a 99999999");}*/
     }
 
     public String getNombres() {
@@ -43,22 +74,30 @@ class Cliente extends Usuario {
 
     public void setNombres(String nombres) {
         this.nombres = nombres;
+/*        if (validarNombres(nombres)) {
+            this.nombres = nombres;
+        } else {System.out.println("Ingresa bien los nombres. Utiliza un total mínimo de 5 caracteres y máximo de 30 caracteres");}*/
     }
-
     public String getApellidos() {
         return apellidos;
     }
 
     public void setApellidos(String apellidos) {
         this.apellidos = apellidos;
+/*        if (validarLongitudApellidos(apellidos)) {
+            this.apellidos = apellidos;
+        } else {System.out.println("Ingresa bien los apellidos. Utiliza un total mínimo de 5 caracteres y máximo de 30 caracteres");}*/
     }
 
-    public String getTelefono() {
+    public int getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(String telefono) {
+    public void setTelefono(int telefono) {
         this.telefono = telefono;
+/*        if (validarTelefono(telefono)) {
+            this.telefono = telefono;
+        } else {System.out.println("Ingresa bien el teléfono. Es obligatorio");}*/
     }
 
     public String getAfp() {
@@ -67,6 +106,9 @@ class Cliente extends Usuario {
 
     public void setAfp(String afp) {
         this.afp = afp;
+/*        if (validarLongitudAfp(afp)) {
+            this.afp = afp;
+        } else {System.out.println("Ingresa bien la AFP. Min 4 caracteres, max 30 caracteres");}*/
     }
 
     public int getSistemaSalud() {
@@ -75,6 +117,18 @@ class Cliente extends Usuario {
 
     public void setSistemaSalud(int sistemaSalud) {
         this.sistemaSalud = sistemaSalud;
+/*        if (validarSistemaSalud(sistemaSalud)) {
+            switch (sistemaSalud){
+                case 1:
+                    System.out.println("Fonasa");
+                    this.sistemaSalud = sistemaSalud;
+                    break;
+                case 2:
+                    System.out.println("Isapre");
+                    this.sistemaSalud = sistemaSalud;
+                    break;
+            }
+        } else {System.out.println("Ingresa bien tu sistema de salud. 1: FONASA, 2: ISAPRE");}*/
     }
 
     public String getDireccion() {
@@ -83,6 +137,9 @@ class Cliente extends Usuario {
 
     public void setDireccion(String direccion) {
         this.direccion = direccion;
+/*        if (validarLongitudDireccion(direccion)) {
+            this.direccion = direccion;
+        } else {System.out.println("Ingresa bien tu dirección: máximo 70 caracteres");}*/
     }
 
     public String getComuna() {
@@ -91,6 +148,9 @@ class Cliente extends Usuario {
 
     public void setComuna(String comuna) {
         this.comuna = comuna;
+/*        if (validarLongitudComuna(comuna)) {
+            this.comuna = comuna;
+        } else {System.out.println("Ingresa bien la comuna: máximo 50 caracteres");}*/
     }
 
     public int getEdad() {
@@ -99,11 +159,85 @@ class Cliente extends Usuario {
 
     public void setEdad(int edad) {
         this.edad = edad;
+/*        if (validarEdad(edad)) {
+            this.edad = edad;
+        } else {System.out.println("Ingresa bien la edad: un valor entre 0 y 150 años");}*/
     }
 
+    // VALIDADORES CLASS CLIENTE:
+/*  // VALIDADOR RUT: VALORES ENTRE 0 Y 99999999
+    public static boolean validarRut(int rut) {
+        return rut >= 0 && rut <= 99999999;
+    }
+
+    // VALIDADOR LONGITUD STRING NOMBRES: MIN 5 MAX 30
+    public static boolean validarNombres(String nombres) {
+        return nombres.length() >= 5 && nombres.length() <= 30;
+    }
+
+   // VALIDADOR LONGITUD STRING APELLIDOS: MIN 5, MAX 30
+    public static boolean validarLongitudApellidos(String apellidos) {
+        String patron = "^.{5,30}$";
+        return Pattern.matches(patron, apellidos);
+    }
+
+    // VALIDADOR TELÉFONO
+    public static boolean validarTelefono(int telefono) {
+        String telefonoStr = Integer.toString(telefono);
+        String patron = "^[0-9]{1,}$";
+        return Pattern.matches(patron, telefonoStr);
+    }
+
+    // VALIDADOR LONGITUD STRING AFP: MIN 4, MAX 30
+    public static boolean validarLongitudAfp(String afp) {
+        String patron = "^.{4,30}$";
+        return Pattern.matches(patron, afp);
+    }
+
+    // VALIDADOR SISTEMA DE SALUD: 1 FONASA, 2 ISAPRE
+    public static boolean validarSistemaSalud(int sistemaSalud) {
+        return sistemaSalud == 1 || sistemaSalud == 2;
+    }
+
+    // VALIDADOR DIRECCION: MAX 70
+    public static boolean validarLongitudDireccion(String direccion) {
+        int longitudMaxima = 70;
+        return direccion.length() <= longitudMaxima;
+    }
+
+    // VALIDADOR COMUNA: MAX 50
+    public static boolean validarLongitudComuna(String comuna) {
+        int longitudMaxima = 50;
+        return comuna.length() <= longitudMaxima;
+    }
+
+    // VALIDADOR EDAD: 0 <= edad < 150
+    public static boolean validarEdad(int edad) {
+        return edad >= 0 && edad < 150;
+    }*/
+
+    @Override
+    public void analizarUsuario() {
+        System.out.println("Analizando al cliente: " + getNombre());
+    }
+
+    public void obtenerNombre() {
+
+    }
+
+    public void obtenerSistemaSalud(){
+
+    }
     @Override
     public String toString() {
-        String sistemaSaludStr = (sistemaSalud == 1) ? "Fonasa" : "Isapre";
-        return super.toString() + "\nRUT: " + rut + "\nNombres: " + nombres + "\nApellidos: " + apellidos + "\nTeléfono: " + telefono + "\nAFP: " + afp + "\nSistema de Salud: " + sistemaSaludStr + "\nDirección: " + direccion + "\nComuna: " + comuna + "\nEdad: " + edad;
+        return super.toString() + "\n" +
+                "Apellidos: " + apellidos + "\n" +
+                "Teléfono: " + telefono + "\n" +
+                "AFP: " + afp + "\n" +
+                "Sistema de Salud: " + sistemaSalud + "\n" +
+                "Dirección: " + direccion + "\n" +
+                "Comuna: " + comuna + "\n" +
+                "Edad: " + edad;
     }
+
 }
